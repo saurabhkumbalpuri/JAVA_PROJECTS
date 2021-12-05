101492af0ae1e3be598363788bb45f96e0cc3675
saurabhkumbalpuri <94901937+saurabhkumbalpuri@users.noreply.github.com>
2021-12-05 13:42:06 +0530

    Commit.
    Commit.

---
+++ CUSTOMIZED WELCOME MESSAGE1/src/Main.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+
+        String name = sc.next();
+
+        System.out.println("Hello "+name+"! Welcome to Event Management System.");
+    }
+
+
+}
