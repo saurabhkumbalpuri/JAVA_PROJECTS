c201097a5a3fb72d1112c8a2bb377e514d7c9e07
saurabhkumbalpuri <94901937+saurabhkumbalpuri@users.noreply.github.com>
2021-12-05 13:35:10 +0530

    commit
    commit

---
+++ Article count/src/Article.java
@@ -0,0 +1,34 @@
+  import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Article extends Thread{
+    String line;
+	 int count;
+	 
+	 public Article() {
+		 super();
+	 }
+	 public Article(String line) {
+		 super();
+		 
+		 this.line = line;
+	 }
+	 public String getLine() {
+		 return line;
+	 }
+	 public void setLine(String line) {
+		 this.line=line;
+	 }
+	 public int getCount() {
+		 return count;
+	 }
+	 public void setCount(int count) {
+		 this.count=count;
+	 }
+	 public void run() {
+		Matcher m = Pattern.compile("(?i)\\b((a)|(an)|(the))\\b").matcher(this.getLine());
+		 while(m.find()) {
+			 this.count++;
+		 }
+	 }
+}
---
+++ Article count/src/Main.java
@@ -0,0 +1,26 @@
+
+ 
+ import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args)throws InterruptedException{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of lines");
+		int n=sc.nextInt();
+		int count=0;
+		sc.nextLine();
+		for(int i=0;i<n;i++) {
+			Article a = new Article();
+			System.out.println("Enter line "+(i+1));
+			a.setLine(sc.nextLine());
+			a.start();
+			a.join();
+			try{count+=a.getCount();
+			}catch(Exception e) {
+				
+			}
+		}
+		System.out.println("There are " + count +  " articles in the given input");
+		
+		}
+}
