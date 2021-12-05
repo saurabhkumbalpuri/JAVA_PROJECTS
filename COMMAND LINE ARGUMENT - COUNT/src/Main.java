abac9551c3493487fa0cceda15e20f0d06d65107
saurabhkumbalpuri <94901937+saurabhkumbalpuri@users.noreply.github.com>
2021-12-05 13:36:59 +0530

    commit
    commit

---
+++ COMMAND LINE ARGUMENT - COUNT/src/Main.java
@@ -0,0 +1,15 @@
+ import java.util.Scanner;
+ 
+ 
+ public class Main{
+	 public static void main (String [] args) {
+		 int i;
+		 System.out.println(" Arguments :");
+		 for(i=0;i<args.length;i++) {
+			 System.out.println(args[i]);
+			 
+		 }
+		 System.out.println("The number of arguments is " + (i));
+		 }
+	 }
+ 
