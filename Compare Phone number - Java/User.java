fa887f0b336a8ff58606e406260d33b2f7744c2f
saurabhkumbalpuri <94901937+saurabhkumbalpuri@users.noreply.github.com>
2021-12-05 13:38:22 +0530

    commit
    commit

---
+++ Compare Phone number - Java/User.java
@@ -0,0 +1,13 @@
+public class User {
+
+        //Fill your code
+	
+    boolean flag = false;
+	public boolean comparePhoneNumber(User user)	{
+		
+		//Fill your code
+        return flag;
+
+	}
+	
+}
