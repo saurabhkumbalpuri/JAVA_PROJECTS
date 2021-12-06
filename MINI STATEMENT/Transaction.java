583655f4604cd0e96c13760bf3d38278468668fa
saurabhkumbalpuri <94901937+saurabhkumbalpuri@users.noreply.github.com>
2021-12-06 11:53:54 +0530

    commit
    commit

---
+++ MINI STATEMENT/Transaction.java
@@ -0,0 +1,22 @@
+
+public class Transaction {
+	private String accountNumber;
+	private Double amount;
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	//fill your code here
+	boolean validate(Double transactionAmount) {
+		// fill your code here
+	}
+
+}
