a9ac0f18a6bb443d477e0fc686bd772a32dbf228
saurabhkumbalpuri <94901937+saurabhkumbalpuri@users.noreply.github.com>
2021-12-06 16:25:01 +0530

    commit
    commit

---
+++ SERIES 1ebox/src/Main.java
@@ -0,0 +1,26 @@
+
+ import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+		//Fill your code here
+    	
+    	
+    	
+    	
+    	System.out.println("");
+    	Scanner sc = new Scanner (System.in);
+    	 int n=sc.nextInt();
+    	 int arr []=new int[n];
+    	 for (int i=1 ; i<=arr.length;i++)
+    	 {
+    		 int a = i*i;
+    		 System.out.print(a+" ");
+    	 }
+	}
+}
