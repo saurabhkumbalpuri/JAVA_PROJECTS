88e89974929850777e7f334adbf5464763823916
saurabhkumbalpuri <94901937+saurabhkumbalpuri@users.noreply.github.com>
2021-12-06 16:13:25 +0530

    commit
    commit

---
+++ PATTERN 35/src/Main.java
@@ -0,0 +1,19 @@
+
+ import java.util.Scanner;
+ 
+ public class Main{
+public static void main (String[] args) {
+	Scanner sc = new Scanner (System.in);
+	int rows = sc.nextInt();
+	for(int i = rows;i>=1;i--) {
+		for(int j=i;j>=1;j--) {
+			System.out.printf(j+" ");
+		}
+		
+		System.out.println();
+	}
+	
+	sc.close();
+	
+}
+}
