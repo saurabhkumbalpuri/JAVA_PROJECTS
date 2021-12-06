c0727ff16e152e312fdb1c7f830c3c3190176d59
saurabhkumbalpuri <94901937+saurabhkumbalpuri@users.noreply.github.com>
2021-12-06 16:42:10 +0530

    commit
    commit

---
+++ WRAPPER CLASS – 1/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+		// fill your code here
+    	System.out.println("Maximum exponent: "+Float.MAX_EXPONENT);
+    	System.out.println("Maximum value: "+Float.MAX_VALUE);
+    	System.out.println("Number of bits: "+Float.SIZE);
+    	
+	}
+}
