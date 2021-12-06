f13b1daa409951f31a66039d4bf81fca267e21d8
saurabhkumbalpuri <94901937+saurabhkumbalpuri@users.noreply.github.com>
2021-12-06 16:17:04 +0530

    commit.
    commit.

---
+++ Rectangle Dimension Change - instanceof operator/src/Main.java
@@ -0,0 +1,41 @@
+
+ import java.io.*; 
+import java.util.*; 
+import java.lang.*;
+public class Main{ 
+	public static void main (String[] args) throws Exception{ 
+
+		//Fill your code
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter n :");
+		int n=sc.nextInt();
+		System.out.println("Enter numbers : ");
+		String a[] = new String[n];
+		int[] arr;
+	    arr = new int[n];
+        for (int i=0;i<n;i++)
+        {
+        	
+           arr[i]=sc.nextInt();   
+	    }
+        System.out.println("Enter strings : ");
+        for (int i=0;i<n;i++)
+        {
+        	
+        	 a[i] = sc.next();
+        	 
+        	
+        }
+        System.out.println("Displaying numbers");
+        for (int var : arr)
+        { 
+           System.out.println(var);
+        }
+        System.out.println("Displaying strings");
+        for (String str : a) 
+        {
+        	System.out.println(str);	
+        }
+        
+	} 
+}
---
+++ Rectangle Dimension Change - instanceof operator/src/Rectangle.java
@@ -0,0 +1,9 @@
+
+public class Rectangle {
+
+	public Rectangle() {
+		
+		 
+	}
+
+}
