7858cae7a4947bb96dbb29a5736e8d167f669f2c
saurabhkumbalpuri <94901937+saurabhkumbalpuri@users.noreply.github.com>
2021-12-06 16:34:57 +0530

    commit.
    commit.

---
+++ Star Pattern/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+	public static void main(String[] args) throws Exception{ 
+		
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<=i;j++) 
+			{
+				System.out.print("*");
+			}
+			System.out.println( " ");
+		}
+		sc.close();
+		
+
+ //		Fill your code
+
+	}
+}
